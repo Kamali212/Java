d51c961aa04d015e97685816570df3be46ef63da
Kamali <Kamalikh3@gmail.com>
2025-01-24 21:01:14 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ console.log(typeof employeeName); //logging employees name and string type to th
 console.log(typeof employeeID); // logging the employee id and string type to the console
 console.log(typeof isActive); // logging employee status and string type to the console
 
+// Task 2 Primitive Data Tyoes
+let productName = "Boots"; // assigning a variable for product name and assigning it a string value
+const productPrice = 650; // assigning a variable for product price and assigning it a numeric value
+var productquantity = 13; // assigning a variable for product quantity and assigning it a numeric value
+console.log(typeof productName); // logging product name and string type to the console
+console.log(typeof productPrice); // logging product price and string type to the console
+console.log(typeof productquantity); // logging product quantity and string type to the console
+
