84f01660aed817467e51a656e76b6e1a585f1be8
Kamali <Kamalikh3@gmail.com>
2025-01-24 21:00:20 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1 Variables
+let employeeName = "Padme Amidala"; // assigning a variable for employees name and assigning it a string value
+const employeeID = 0; // assigning a variable for employee id and assigning it a numeric value
+var isActive = true; // assigning a variable for employee status and assigning it a boolean value
+console.log(typeof employeeName); //logging employees name and string type to the console
+console.log(typeof employeeID); // logging the employee id and string type to the console
+console.log(typeof isActive); // logging employee status and string type to the console
+
